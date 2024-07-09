452eb8ed7373c59588558a784e47c9c2d0366994
ztwww2222 <130041523+ztwww2222@users.noreply.github.com>
2024-07-09 09:34:33 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ fi
 }
 
 install_config2(){
-processes=("nginx.js" "bot.js" "cff.js")
+processes=("web.js" "nezha.js" "cff.js" "app")
 for process in "${processes[@]}"
 do
     pid=$(pgrep -f "$process")
@@ -263,7 +263,7 @@ esac
 
 echo "等待脚本启动...如果等待时间过长，可能是判断不准确，实际已经成功，可以通过观察哪吒自行判断或重启尝试"
 sleep 15
-keyword="bot.js"
+keyword="web.js"
 max_attempts=5
 counter=0
 
@@ -344,7 +344,7 @@ case $choice in
         nohup ${FLIE_PATH}start.sh 2>/dev/null 2>&1 &
 echo "等待脚本启动...，如果等待时间过长，可能是判断不准确，实际已经成功，可以通过观察哪吒自行判断"
 sleep 15
-keyword="bot.js"
+keyword="web.js"
 max_attempts=5
 counter=0
 
@@ -426,7 +426,7 @@ if [ "$(systemctl is-active my_script.service)" == "active" ]; then
     systemctl stop my_script.service
     echo "Service stopped."
 fi
-processes=("bot.js" "nginx.js" "app.js" "cff.js" "nezha.js")
+processes=("web.js" "app" "app.js" "cff.js" "nezha.js")
 for process in "${processes[@]}"
 do
     pid=$(pgrep -f "$process")
@@ -487,7 +487,7 @@ if [ -d "/tmp/worlds/" ]; then
 rm -rf /tmp/worlds/
 fi
 
-processes=("nginx.js" "bot.js" "cff.js" "nezha.js" "app.js")
+processes=("app" "web.js" "cff.js" "nezha.js" "app.js")
 for process in "${processes[@]}"
 do
     pid=$(pgrep -f "$process")
