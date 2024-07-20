4c57fde0c4a0cb8aceb1d137b1125a615ca7155f
ztwww2222 <130041523+ztwww2222@users.noreply.github.com>
2024-07-21 01:01:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ export TMP_ARGO=${TMP_ARGO:-'rel'}
 if [ "${TMP_ARGO}" == "rel" ]; then 
 echo -n "请输入节点端口(默认443，注意nat鸡端口不要超过范围):"
 read SERVER_PORT
-SERVER_PO=${SERVER_PORT:-"443"}
+SERVER_POT=${SERVER_PORT:-"443"}
 
 echo -n "请输入 节点名称（默认值：vps）: "
 read SUB_NAME
